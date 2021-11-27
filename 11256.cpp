812a643b59905ad6c243db8b3c809872411a0149
kingh2160 <38389986+kingh2160@users.noreply.github.com>
2021-11-28 01:12:07 +0900

    Create 11256.cpp

---
+++ 11256.cpp
@@ -0,0 +1,62 @@
+ #include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <list>
+#include <deque>
+#include <numeric>
+#include <set>
+#include <climits>
+#include <utility>
+#include <map>
+#include <float.h>
+typedef long long ll;
+typedef unsigned long long ull;
+inline int max( int x, int y ){ return x > y ? x : y ; }
+inline int min( int x, int y ){ return x < y ? x : y ; }
+inline ll max( ll x, ll y ){ return x > y ? x : y ; }
+inline ll min( ll x, ll y ){ return x < y ? x : y ; }
+inline ull max( ull x, ull y ){ return x > y ? x : y ; }
+inline ull min( ull x, ull y ){ return x < y ? x : y ; }
+ull GCD( ull a, ull b ) { return b ? GCD( b , a%b ) : a; }
+#define	ABS(x) ( ((x)<0)?-(x):(x) )
+using namespace std;
+#define INF 1e9
+#define MOD 1e9+7
+#define PIV 3.14159265358979323846
+#define MAXN 200000
+typedef pair<char, int> P;
+const int MAX_V = 50;
+
+int main(void) {
+    
+    int T, N, J, A, B, cnt;
+    cin >> T;
+    
+    while( T-- ){
+        priority_queue<int> pq;
+        scanf("%d %d", &J, &N);
+        for( int i=0; i<N; ++i ){
+            scanf("%d %d", &A, &B);
+            pq.push(A*B);
+        }
+        cnt = 0;
+        while( J > 0 ){
+            J -= pq.top();
+            pq.pop();
+            cnt++;
+        }
+        printf("%d\n", cnt);
+    }
+    
+    return 0;
+}
