a02d2461915db942dcc9a68e6b8cae4636cfedfd
kingh2160 <38389986+kingh2160@users.noreply.github.com>
2021-11-27 20:15:05 +0900

    Create 16953.cpp

---
+++ 16953.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <list>
+#include <deque>
+#include <numeric>
+#include <set>
+#include <climits>
+#include <utility>
+#include <map>
+#include <float.h>
+typedef long long ll;
+typedef unsigned long long ull;
+inline int max( int x, int y ){ return x > y ? x : y ; }
+inline int min( int x, int y ){ return x < y ? x : y ; }
+inline ll max( ll x, ll y ){ return x > y ? x : y ; }
+inline ll min( ll x, ll y ){ return x < y ? x : y ; }
+inline ull max( ull x, ull y ){ return x > y ? x : y ; }
+inline ull min( ull x, ull y ){ return x < y ? x : y ; }
+ull GCD( ull a, ull b ) { return b ? GCD( b , a%b ) : a; }
+using namespace std;
+#define INF 1e9
+#define MOD 1e9+7
+#define PIV 3.14159265358979323846
+#define MAXN 200000
+typedef pair<int, int> P;
+const int MAX_V = 50;
+
+int main(void) {
+    
+    int A, B;
+    int ans = 0;
+    
+    scanf("%d %d", &A, &B);
+    
+    while( A < B ){
+        if( B % 10 == 1 ){
+            B /= 10;
+            ans++;
+        }else if( B%2 == 0 ){
+            B /= 2;
+            ans++;
+        }else{
+            if( A != B ){
+                printf("-1");
+                return 0;
+            }
+        }
+    }
+    
+    if( A == B ) printf("%d", ans+1);
+    else printf("-1");
+    return 0;
+}
