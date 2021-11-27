8e33a531ea88122e083c0cb160236f79ccd35f56
kingh2160 <38389986+kingh2160@users.noreply.github.com>
2021-11-27 23:34:21 +0900

    Create 21313.cpp

---
+++ 21313.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <list>
+#include <deque>
+#include <numeric>
+#include <set>
+#include <climits>
+#include <utility>
+#include <map>
+#include <float.h>
+typedef long long ll;
+typedef unsigned long long ull;
+inline int max( int x, int y ){ return x > y ? x : y ; }
+inline int min( int x, int y ){ return x < y ? x : y ; }
+inline ll max( ll x, ll y ){ return x > y ? x : y ; }
+inline ll min( ll x, ll y ){ return x < y ? x : y ; }
+inline ull max( ull x, ull y ){ return x > y ? x : y ; }
+inline ull min( ull x, ull y ){ return x < y ? x : y ; }
+ull GCD( ull a, ull b ) { return b ? GCD( b , a%b ) : a; }
+using namespace std;
+#define INF 1e9
+#define MOD 1e9+7
+#define PIV 3.14159265358979323846
+#define MAXN 200000
+typedef pair<char, int> P;
+const int MAX_V = 50;
+
+int main(void) {
+    
+    int N;
+    
+    cin >> N;
+    for( int i=0; i<N/2; ++i ) printf("1 2 ");
+    if( N&0x1 ) printf("3");
+    return 0;
+}
