3b104c591c89ec432fd8be141af23a0d68cee6ad
kingh2160 <38389986+kingh2160@users.noreply.github.com>
2021-12-07 22:22:40 +0900

    Create 5525.cpp

---
+++ 5525.cpp
@@ -0,0 +1,62 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <list>
+#include <deque>
+#include <numeric>
+#include <set>
+#include <climits>
+#include <utility>
+#include <map>
+#include <float.h>
+typedef long long ll;
+typedef unsigned long long ull;
+inline int max( int x, int y ){ return x > y ? x : y ; }
+inline int min( int x, int y ){ return x < y ? x : y ; }
+inline ll max( ll x, ll y ){ return x > y ? x : y ; }
+inline ll min( ll x, ll y ){ return x < y ? x : y ; }
+inline ull max( ull x, ull y ){ return x > y ? x : y ; }
+inline ull min( ull x, ull y ){ return x < y ? x : y ; }
+//ull GCD( ull a, ull b ) { return b ? GCD( b , a%b ) : a; }
+int GCD( int a, int b ) { return b ? GCD( b , a%b ) : a; }
+#define	ABS(x) ( ((x)<0)?-(x):(x) )
+using namespace std;
+#define INF 1e9+7
+#define MOD 1e9+7
+#define PIV 3.14159265358979323846
+#define MAXN 200000
+typedef pair<int, int> P;
+const int MAX_V = 50;
+
+int main(){
+    int N, M;
+    int pattern = 0;
+    int ans = 0;
+    string target = "";
+    
+    scanf("%d", &N);
+    scanf("%d", &M);
+    cin >> target;
+    for( int i=0; i<M; ++i ){
+        if( target[i] == 'I' && target[i+1] == 'O' && target[i+2] == 'I' ){
+            pattern++;
+            if( N == pattern ){
+                pattern--;
+                ans++;
+            }
+            i++;
+        }else pattern = 0;
+    }
+    printf("%d", ans);
+    return 0;
+}
