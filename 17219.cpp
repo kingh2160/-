0cacb9b11df43d1bc3004f14fb4a283083f7ed3a
kingh2160 <38389986+kingh2160@users.noreply.github.com>
2021-12-07 22:47:55 +0900

    Create 17219.cpp

---
+++ 17219.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <list>
+#include <deque>
+#include <numeric>
+#include <set>
+#include <climits>
+#include <utility>
+#include <map>
+#include <float.h>
+typedef long long ll;
+typedef unsigned long long ull;
+inline int max( int x, int y ){ return x > y ? x : y ; }
+inline int min( int x, int y ){ return x < y ? x : y ; }
+inline ll max( ll x, ll y ){ return x > y ? x : y ; }
+inline ll min( ll x, ll y ){ return x < y ? x : y ; }
+inline ull max( ull x, ull y ){ return x > y ? x : y ; }
+inline ull min( ull x, ull y ){ return x < y ? x : y ; }
+//ull GCD( ull a, ull b ) { return b ? GCD( b , a%b ) : a; }
+int GCD( int a, int b ) { return b ? GCD( b , a%b ) : a; }
+#define	ABS(x) ( ((x)<0)?-(x):(x) )
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+#define INF 1e9+7
+#define MOD 1e9+7
+#define PIV 3.14159265358979323846
+#define MAXN 200000
+typedef pair<int, int> P;
+const int MAX_V = 50;
+
+int main(){
+    map<string, string> m;
+    int N, M;
+    string str1, str2;
+    
+    fastio;
+    cin >> N >> M;
+    for( int i=0; i<N; ++i ){
+        cin >> str1 >> str2;
+        m[str1] = str2;
+    }
+    for( int i=0; i<M; ++i ){
+        cin >> str1;
+        cout << m[str1] << '\n';
+    }
+    return 0;
+}
